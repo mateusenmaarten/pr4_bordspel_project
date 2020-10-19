c24f4d3039841da834d4619fe7eca6036f607c28
Maarten Mateusen <mateusenmaarten@hotmail.com>
2020-10-19 19:50:44 +0200

    Initial Commit project creation

---
+++ src/main/java/be/thomasmore/graduaten/pr4_bordspel_project/Pr4BordspelProjectApplication.java
@@ -0,0 +1,13 @@
+package be.thomasmore.graduaten.pr4_bordspel_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pr4BordspelProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Pr4BordspelProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/thomasmore/graduaten/pr4_bordspel_project/Pr4BordspelProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package be.thomasmore.graduaten.pr4_bordspel_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pr4BordspelProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
